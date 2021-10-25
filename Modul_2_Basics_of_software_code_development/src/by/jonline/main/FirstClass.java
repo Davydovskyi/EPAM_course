173d3c78283551bf515ec76dcf24f3d243a10b52
Davydovskyi <92328863+Davydovskyi@users.noreply.github.com>
2021-10-25 21:46:00 +0300

    Add files via upload

---
+++ Modul_2_Basics_of_software_code_development/src/by/jonline/main/FirstClass.java
@@ -0,0 +1,13 @@
+package by.jonline.main;
+
+public class FirstClass {
+	
+	
+	public static void main(String[] args) {
+		
+		Integer a = null;
+		
+		System.out.println(a instanceof Integer);
+		
+	}
+}

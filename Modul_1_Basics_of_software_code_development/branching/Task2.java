a156ecbd390d5e8fdd8eb7f96b1a054a8eed2659
Davydovskyi <92328863+Davydovskyi@users.noreply.github.com>
2021-11-11 20:41:37 +0300

    Add files via upload

---
+++ Modul_1_Basics_of_software_code_development/branching/Task2.java
@@ -0,0 +1,36 @@
+package by.jonline.branching;
+
+/**
+ * Найти max{min(a, b), min(c, d)}.
+ */
+
+public class Task2 {
+
+	public static void main(String[] args) {
+
+		double a = 78;
+		double b = 78.5;
+		double c = 4;
+		double d = 45;
+
+		double result;
+
+		result = max(min(a, b), min(c, d));
+
+		System.out.println("result: " + result);
+
+	}
+
+	public static double max(double a, double b) {
+
+		return a > b ? a : b;
+
+	}
+
+	public static double min(double a, double b) {
+
+		return a < b ? a : b;
+
+	}
+
+}

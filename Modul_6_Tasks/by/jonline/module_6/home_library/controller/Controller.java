1706f766fbe9a92448cfa300009051d3b9799b17
Davydovskyi <anton.davydoone@mail.ru>
2022-08-29 17:59:45 +0300

    Update Controller.java

--- Modul_6_Tasks/by/jonline/module_6/home_library/controller/Controller.java
+++ Modul_6_Tasks/by/jonline/module_6/home_library/controller/Controller.java
@@ -2,7 +2,7 @@ package by.jonline.module_6.home_library.controller;
 
 public interface Controller {
 
-	String DELIMETER = "'\\s+'";
+	String DELIMETER = "' '";
 
 	String doAction(String request);
 }

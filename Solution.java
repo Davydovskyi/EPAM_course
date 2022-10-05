67cbc1875c3f03f4f067d0d6e36a1b48f73a8b1e
Davydovskyi <anton.davydoone@mail.ru>
2022-10-05 13:34:26 +0300

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task22.task2202;
+
+/* 
+Найти подстроку
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(getPartOfString("JavaRush - лучший сервис обучения Java."));
+    }
+
+    public static String getPartOfString(String string) {
+        return string.substring(string.indexOf(" ") + 1);
+    }
+
+    public static class TooShortStringException {
+    }
+}
